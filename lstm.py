f15e72884afc23e1dc48c35c070bb6d37ae8ea62
cnzx05cnzx <55571432+cnzx05cnzx@users.noreply.github.com>
2022-07-19 14:10:40 +0800

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -26,3 +26,41 @@ class LSTM(nn.Module):
         out = self.linear(out)
 
         return out
+
+    class LSTM_with_Attention(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim,
+                 n_layers=1, use_bidirectional=False, use_dropout=False):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim // 2,
+                           bidirectional=use_bidirectional,
+                           dropout=0.5 if use_dropout else 0.)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(0.5 if use_dropout else 0.)
+
+    def attention_net(self, lstm_output, final_state):
+        lstm_output = lstm_output.permute(1, 0, 2)
+        hidden = final_state.squeeze(0)
+        attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
+        soft_attn_weights = F.softmax(attn_weights, dim=1)
+        new_hidden_state = torch.bmm(lstm_output.transpose(1, 2),
+                                     soft_attn_weights.unsqueeze(2)).squeeze(2)
+
+        return new_hidden_state
+
+    def attention(self, lstm_output, final_state):
+        lstm_output = lstm_output.permute(1, 0, 2)
+        merged_state = torch.cat([s for s in final_state], 1)
+        merged_state = merged_state.squeeze(0).unsqueeze(2)
+        weights = torch.bmm(lstm_output, merged_state)
+        weights = F.softmax(weights.squeeze(2), dim=1).unsqueeze(2)
+        return torch.bmm(torch.transpose(lstm_output, 1, 2), weights).squeeze(2)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        output, (hidden, cell) = self.rnn(embedded)
+
+        # attn_output = self.attention_net(output, hidden)
+        attn_output = self.attention(output, hidden)
+
+        return self.fc(attn_output.squeeze(0))
